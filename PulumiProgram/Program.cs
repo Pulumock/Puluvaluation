c5b980f9f50566c6cc36c2e2ed1e6e4bbfb0462e
Albin <albinronnkvist@protonmail.com>
2025-05-05 14:01:43 +0200

    Pragma disable ConfigureAwait

--- PulumiProgram/Program.cs
+++ PulumiProgram/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Pulumi;
 using PulumiProgram;
 
+#pragma warning disable CA2007
 return await Deployment.RunAsync(async () => await CoreStack.DefineResourcesAsync());
+#pragma warning restore CA2007
